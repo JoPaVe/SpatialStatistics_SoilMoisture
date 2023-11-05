47fa031a3dc9cb504dbb6255a4fe664457cc02fc
Jonas Verch <jonas.verch@web.de>
2023-11-05 11:03:05 -0600

    Added loading SAD regions

--- DataLoading.R
+++ DataLoading.R
@@ -2,13 +2,11 @@ library(terra)
 library(ggplot2)
 library(dplyr)
 library(plyr)
-
-
+library(sf)
+library(parallel)
+library(raster)
 
 path <- paste0(getwd(),"/data",sep="")
-
-moisture_data <- load_tif_pictures(path, file_type)
-cropyield_data <- load_cropyield_data(path)
 
 ############## Load crop yield data (Canada Agriculture) ###############
 
@@ -34,13 +32,11 @@ load_cropyield_list <- function(files_name_vec) {
 
 ############## Load TIF Pictures (SMOS data) ###############
 
-file_type = NULL
-
 load_tif_pictures <- function(initial_path, file_type) {
   ## Load all tif pictures and store them in a list of data frames for each year
   ## Output -> list(year): dataframe(x,y,date1,date2,....)
   
-  files_vec <- extract_files_vector(initial_path, file_type = NULL) #Vector of files in directory
+  files_vec <- extract_files_vector(initial_path, file_type = file_type) #Vector of files in directory
   list_dates_data <- create_list_dates_data(files_vec)
   return(list_dates_data)
 }
@@ -70,6 +66,14 @@ create_list_dates_data <- function(files_vec) {
 
 ####
 
+############## Load small_area_data_regions.gdb ###############
+gdb_path <- paste0(path,"/small_area_data_regions.gdb")
+
+canada_layers <- st_layers(dsn = gdb_path)
+
+SADRegions2017 <- st_read(gdb_path, layer = "SADRegionsRDPI_2017")
+SADRegions2008_2015 <- st_read(gdb_path, layer = "SADRegionsRDPI_2008_2015")
+
 ##########################################################################
 
 ############## General Functions ##############
@@ -80,3 +84,8 @@ extract_files_vector <- function(initial_path, file_type = NULL, recursive_set =
     files <- files[grep(file_type, files, ignore.case = TRUE)]
   return(files)
 }
+
+file_type = ".tif"
+
+moisture_data <- load_tif_pictures(path, ".tif")
+cropyield_data <- load_cropyield_data(path)
