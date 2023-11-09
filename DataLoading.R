5619b796b714636558333be859d4607ebf7d3ffb
Jonas Verch <jonas.verch@web.de>
2023-11-08 21:30:58 -0600

    Split create_list_dates_data into 2 functions

--- DataLoading.R
+++ DataLoading.R
@@ -37,33 +37,43 @@ load_tif_pictures <- function(initial_path, file_type) {
   ## Output -> list(list(year): dataframe(x,y,date1,date2,....), rast_objects): 
   
   files_vec <- extract_files_vector(initial_path, file_type = file_type) #Vector of files in directory
-  list_dates_data <- create_list_dates_data(files_vec)
-  return(list_dates_data)
+  year_df_list <- create_list_dates_data(files_vec) # returns list of files_lists and rast_objects
+  list_dates_data <- merge_year_df(year_df_list[[1]]) # returns list of data_frames
+  return(list(list_dates_data, year_df_list[[2]]))
 }
 
 ####
 
 create_list_dates_data <- function(files_vec) {
-  files_list <- list()
-  rast_objects <- list()
-  
+
   ## Rewrite for lapply
-  for (file_path in files_vec) {
-    year_match <- regmatches(file_path,regexpr("\\d{4}",file_path))
-    date_match <- regmatches(file_path,regexpr("\\d{8}",file_path))
-    rast_obj <- rast(file_path)
-    rast_df <- as.data.frame(rast_obj, xy = T)
-    colnames(rast_df)[3] <- date_match
-    files_list[[year_match]][[date_match]] <- rast_df
-    rast_objects[[date_match]] <- rast_obj
+  if(!exists("rast_objects") || !exists("files_list")) {
+    
+    files_list <- list()
+    rast_objects <- list()
+    
+    for (file_path in files_vec) {
+      year_match <- regmatches(file_path,regexpr("\\d{4}",file_path))
+      date_match <- regmatches(file_path,regexpr("\\d{8}",file_path))
+      rast_obj <- rast(file_path)
+      rast_df <- as.data.frame(rast_obj, xy = T)
+      colnames(rast_df)[3] <- date_match
+      files_list[[year_match]][[date_match]] <- rast_df
+      rast_objects[[date_match]] <- rast_obj
+      print(date_match)
+    }
   }
-  
+  return(list(files_list,rast_objects))
+}
+
+merge_year_df <- function(year_df_list) {
   year_df_list <- lapply(files_list, function(sublist) {
+    print(sublist)
     year_df <- Reduce(function(df1,df2) merge(df1,df2,by = c("x","y"),all = T), sublist)
     return(year_df)
   })
 
-  return(list(year_df_list,rast_objects))  
+  return(year_df_list)  
 }
 
 ####
@@ -89,6 +99,6 @@ extract_files_vector <- function(initial_path, file_type = NULL, recursive_set =
 
 
 file_type = ".tif"
-
+debug(load_tif_pictures)
 moisture_objects <- load_tif_pictures(path, ".tif")
 cropyield_data <- load_cropyield_data(path)
