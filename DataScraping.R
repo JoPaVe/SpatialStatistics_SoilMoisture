39161933340123b82b86caf285c602730625aa3d
Jonas Verch <jonas.verch@web.de>
2023-11-08 20:56:17 -0600

    Bugfix years in all functions

--- DataScraping.R
+++ DataScraping.R
@@ -20,9 +20,7 @@ extract_all_datafiles <- function(years, start_link) {
 create_sub_folders <- function(years) {
   ## Create subfolders for each year in working directory in ./data/'year'.
   
-  years_seq <- seq(years[1],years[length(years)[2]],1)
-  
-  for (year in years_seq) {
+    for (year in years) {
     dir.create(paste0(getwd(),"/data/",year,sep=""))
   }
   
@@ -31,8 +29,8 @@ create_sub_folders <- function(years) {
 generate_daily_links <- function(years, start_link) {
   ## Create vector of links for the daily data for each year.
   
-  years <- paste0(seq(years[1],years[dim(years)[2]],1))
-  daily_links <- paste0(link,years,"/daily/")
+  
+  daily_links <- paste0(link,paste0(years),"/daily/")
   return(daily_links)
 }
 
@@ -40,7 +38,6 @@ read_download_links <- function(years, daily_links) {
   ## Create vector of download links for each file that should be downloaded.
   
   return_list <- list()
-  years <- seq(years[1],years[2],1)
   
   for (year_count in 1:length(years)) {
     start_page_year <- read_html(daily_links[year_count])
@@ -67,6 +64,6 @@ run_downloads <- function(years, download_links_list) {
     }, folder_year = names(download_links_list))
   })
 } 
-debug(extract_all_datafiles)
+
 extract_all_datafiles(c(2012,2013,2014), link)
 
